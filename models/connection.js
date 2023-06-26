9b03babd2d3cc74bbf0b94c2cdaafa8e800f0c24
Yacine <Yacine.lariane@gmail.com>
2023-06-26 15:20:08 +0200

    congigure env for connection string and api Key

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://yacinelariane:lebylkusdu92@cluster0.paptrih.mongodb.net/weatherApp';
+const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
